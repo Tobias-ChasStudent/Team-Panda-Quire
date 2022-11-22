fa81859187e080f7c0cc2362f3e0744dc13d6158
Hampus Dufberg Töttrup <Hampus>
2022-11-22 17:01:00 +0100

    headings working, indent, prettier button bar

--- js/editor.js
+++ js/editor.js
@@ -6,33 +6,20 @@ let editorContents = "";
 const currentDocument = 'idDoc817326123';
 
 const editorButtons = {
-    btnBold: function(){
-        document.execCommand('bold');
-    },
-    btnItalic: function(){
-        document.execCommand('italic');
-    },
-    btnUnorderedList: function(){
-        document.execCommand('insertUnorderedList');
-    },
-    btnOrderedList: function(){
-        document.execCommand('insertOrderedList');
-    },
-    btnH1: function(){
-        document.execCommand('formatBlock', false, "H1");
-    },
-    btnH2: function(){
-        document.execCommand('formatBlock', false, "H2");
-    },
-    btnH3: function(){
-        document.execCommand('formatBlock', false, "H3");
-    }
+    btnBold: ['bold'],
+    btnItalic: ['italic'],
+    btnUnorderedList: ['insertUnorderedList'],
+    btnOrderedList: ['insertOrderedList'],
+    btnIndent: ['indent'],
+    btnH1: ['formatBlock', false, "H1"],
+    btnH2: ['formatBlock', false, "H2"],
+    btnH3: ['formatBlock', false, "H3"]
 }
 
 const eventListenerToEditorBtns = function () {
     for (const btn of btnsEditor) {
         const id = btn.getAttribute('id');
-        btn.addEventListener('click', editorButtons[id])
+        btn.addEventListener('click', function() {document.execCommand(...editorButtons[id])});
     }
 }
 
